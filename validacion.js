d0766197a70615d42ae27162a60a147d59d8b63c
Nelson Vilte <nelsonvilte@gmail.com>
2022-09-07 01:04:52 -0300

    update validacion inputs y estilo de textarea en formulario

--- validacion.js
+++ validacion.js
@@ -1,1 +1,25 @@
-//Haz tú validación en javascript acá
+/* export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+} */
+
+const inputNombre = document.querySelector("#inputName");
+
+inputNombre.addEventListener("blur", (evento) => {
+  validarNombre(evento.target);
+});
+
+function validarNombre(input) {
+  const nombre = new String(input.value);
+  let mensaje = "";
+  if (!nombreCorrecto(nombre)) {
+    mensaje = "debes ingresar como maximo 50 caracteres";
+  }
+  input.setCustomValidity(mensaje);
+}
+
+function nombreCorrecto(cadena) {
+  return cadena.length < 50;
+}
